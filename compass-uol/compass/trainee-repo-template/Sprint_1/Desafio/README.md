a00f56fd77cc7cbc683cdcd44b0453a005f2c127
Jhoann Feitosa <cjhooann@gmail.com>
2024-09-23 09:06:58 -0300

    Add files via upload

---
+++ compass-uol/compass/trainee-repo-template/Sprint_1/Desafio/README.md
@@ -0,0 +1,7 @@
+## 1- Os comandos que utilizei dentro do arquivo processamento_de_vendas.sh
+
+![código](<Captura de tela 2024-09-19 233422.png>)
+
+## 2- O comando que usei para executar o crontab
+
+![crontab](<Captura de tela 2024-09-19 232646.png>)
---
+++ compass-uol/compass/trainee-repo-template/Sprint_1/Desafio/ecommerce/consolidador_de_processamento_de_vendas.sh
@@ -0,0 +1,4 @@
+cp vendas/backup/relatorio19-09-2024.txt vendas/backup/relatorio_final.txt
+cat vendas/backup/relatorio20-09-2024.txt >> vendas/backup/relatorio_final.txt
+cat vendas/backup/relatorio21-09-2024.txt >> vendas/backup/relatorio_final.txt
+cat vendas/backup/relatorio22-09-2024.txt >> vendas/backup/relatorio_final.txt
